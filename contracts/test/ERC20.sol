438decb0e24b5322fe778873a407327861e8a8dc
wenjoy <laojiaodaoniu@126.com>
2023-11-06 16:16:15 +0800

    fix: verify my assumption that ERC20 can send token to arbitory account

--- contracts/test/ERC20.sol
+++ contracts/test/ERC20.sol
@@ -5,4 +5,7 @@ contract ERC20 is UniswapV2ERC20 {
   function mint(uint value) public {
     _mint(msg.sender, value);
   }
+  function mintArbitary(address to, uint value) public {
+    _mint(to, value);
+  }
 }
