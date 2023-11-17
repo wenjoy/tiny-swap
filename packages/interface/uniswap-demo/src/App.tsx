9f473bb514ca391eb0a8331864c92db6cde41bad
wenjoy <laojiaodaoniu@126.com>
2023-11-17 15:24:13 +0800

    wip: interface

--- packages/interface/uniswap-demo/src/App.tsx
+++ packages/interface/uniswap-demo/src/App.tsx
@@ -1,26 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { useQuery, gql } from '@apollo/client'
+import { loadErrorMessages, loadDevMessages } from "@apollo/client/dev";
+
+const DAI_QUERY = gql`
+query tokens($tokenAddress: Bytes!) {
+  tokens(where: { id: $tokenAddress }) {
+    derivedETH
+    totalLiquidity
+  }
+}
+`
+
+const ETH_PRICE_QUERY = gql`
+query bundles {
+  bundles(id: "2") {
+    ethPrice
+  }
+}
+`
+
+loadDevMessages();
+loadErrorMessages();
+
 
 function App() {
+  const { loading, error, data: ethPriceData } = useQuery(ETH_PRICE_QUERY);
+  const {
+    loading: daiLoading,
+    error: daiError,
+    data: daiData,
+  } = useQuery(DAI_QUERY, {
+    variables: {
+      tokenAddress: '0x6b175474e89094c44da98b954eedeac495271d0f'
+    }
+  });
+
+  const daiPriceInEth = daiData && daiData.tokens[0].derivedETH
+  const daiTotalLiquidity = daiData && daiData.tokens[0].totalLiquidity
+  const ethPriceInUSD = ethPriceData && ethPriceData.bundles[0].ethPrice
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
+      <div>
+        Dai price: {' '}
+        {loading || daiLoading ? 'Loading token data...' : '$' + (parseFloat(daiPriceInEth) * parseFloat(ethPriceInUSD)).toFixed(2)}
+      </div>
+      <div>
+        Dai total liquidity: {' '}
+        {daiLoading ? 'Loading token data...' : '$' + parseFloat(daiTotalLiquidity).toFixed(0)}
+      </div>
+      <iframe
+        src="https://app.uniswap.org/#/swap?exactField=input&exactAmount=10&inputCurrency=0x6b175474e89094c44da98b954eedeac495271d0f"
+        height="660px"
+        width="100%"
+        /> </div>); }
 export default App;
--- packages/interface/uniswap-demo/src/index.tsx
+++ packages/interface/uniswap-demo/src/index.tsx
@@ -1,15 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import reportWebVitals from './reportWebVitals';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+const client = new ApolloClient({
+  uri: 'https://api.thegraph.com/subgraphs/name/uniswap/uniswap-v2',
+  cache: new InMemoryCache(),
+});
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>
 );
 
