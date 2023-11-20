87525ffb907f4d06ad853f74c0fb9be6990011e1
wenjoy <laojiaodaoniu@126.com>
2023-11-20 15:46:23 +0800

    feat: add base component with mui

--- packages/ui/src/components/AddLiquidity.tsx
+++ packages/ui/src/components/AddLiquidity.tsx
@@ -1,12 +1,15 @@
-import Panel from './Panel'
+import Button from '@mui/material/Button';
 import TokenForm from './TokenForm'
+import { Container, Typography } from '@mui/material';
 
 function AddLiquidity () {
   return <div className='AddLiquidity'>
-    <Panel>
-      <header>Add liquidity</header>
+    <Container>
+      <Typography variant='h4'>Add liquidity</Typography>
       <TokenForm />
-    </Panel>
+      <TokenForm />
+      <Button variant='outlined'>Add</Button>
+    </Container>
   </div>
 }
 export default AddLiquidity
--- packages/ui/src/components/TokenForm.tsx
+++ packages/ui/src/components/TokenForm.tsx
@@ -1,12 +1,19 @@
-type TokenForm = {
+import { Card, CardContent, MenuItem, Select, TextField, Typography } from '@mui/material';
 
-}
+// type TokenForm = {
+//
+// }
 function TokenForm() {
-  return <div className="TokenForm">
-    <div>
-      <input placeholder='0' />
-    </div>
-  </div>
+  return <Card sx={{margin: "20px"}} variant='outlined'>
+    <CardContent>
+      <TextField id="token0-value" label="Value" />
+      <Select>
+        <MenuItem value="ETH">ETH</MenuItem>
+        <MenuItem value="DAI">DAI</MenuItem>
+      </Select>
+      <Typography>Balance: 0</Typography>
+    </CardContent>
+  </Card>
 }
 
 export default TokenForm
