1518db064e6cfb4924cb5dede62a2d4db9b8fc28
wenjoy <laojiaodaoniu@126.com>
2023-11-21 14:58:48 +0800

    feat: [smart-contracts] deploy factory contract

--- packages/smart-contracs/scripts/deploy.ts
+++ packages/smart-contracs/scripts/deploy.ts
@@ -1,27 +1,17 @@
 import { ethers } from "hardhat";
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+  const [deployer]= await ethers.getSigners();
+  console.log("Deploying contracts with the account:", deployer.address);
 
-  const lockedAmount = ethers.parseEther("0.001");
+  const factory = await ethers.deployContract("UniswapV2Factory", [deployer]);
 
-  const lock = await ethers.deployContract("Lock", [unlockTime], {
-    value: lockedAmount,
-  });
-
-  await lock.waitForDeployment();
-
-  console.log(
-    `Lock with ${ethers.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
-  );
+  console.log("factory address: ", await factory.getAddress());
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
+main()
+.then(() => process.exitCode = 0)
+.catch((error) => {
   console.error(error);
   process.exitCode = 1;
 });
