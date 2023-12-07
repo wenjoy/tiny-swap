516c8ffc0e9b0efcc1e75efc9ba8e193997e6491
wenjoy <laojiaodaoniu@126.com>
2023-12-07 14:07:46 +0800

    fix: input should only allow number

--- packages/ui/src/components/TokenForm.tsx
+++ packages/ui/src/components/TokenForm.tsx
@@ -13,7 +13,7 @@ function TokenForm(
 ) {
   return <Card sx={{ margin: "20px" }} variant='outlined'>
     <CardContent>
-      <TextField placeholder='0.0' id="token0-value" label="Amount" value={tokenValue} onChange={event => onTokenValueChange(event.target.value)} />
+      <TextField placeholder='0.0' id="token0-value" label="Amount" type="number" value={tokenValue} onChange={event => onTokenValueChange(event.target.value)} />
       <Select value={token} onChange={(event) => onTokenChange(event.target.value as TOKEN)}>
         {TOKENS.map(t => <MenuItem value={t} key={t}>{t}</MenuItem>)}
       </Select>
